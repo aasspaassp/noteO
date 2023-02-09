3864dbe1c0e9055a1a17c3b2695cfca59ebd0a0f
aasspaassp <andresfilos94@gmail.com>
2023-02-09 10:21:13 -0600

    Schemas for database

--- mongo.js
+++ mongo.js
@@ -11,7 +11,14 @@ const password = process.argv[2]
 
 //node mongo.js password
 
-const url = `mongodb+srv://andresfilos94:${password}@cluster0.zsxgft3.mongodb.net/noteApp?retryWrites=true&w=majority`
+const url = `mongodb+srv://andresfilos94:${password}@cluster0.zsxgft3.mongodb.net/noteO?retryWrites=true&w=majority`;
+
+mongoose.set('strictQuery',false);
+mongoose.connect(url);
+
+
+//Si hubiera ts habría que hacer un type que fuera igual
+//Schema for js objects
 
 const noteSchema = new mongoose.Schema({
   content: String,
@@ -20,23 +27,36 @@ const noteSchema = new mongoose.Schema({
 }
 )
 
+//Class for JS objects
 const Note = mongoose.model('Note', noteSchema)
 
-mongoose
-  .connect(url)
-  .then((result) => {
-    console.log('connected to MongoDB!')
 
-    const note = new Note({
-      content: "HTML is Easy",
-      date: new Date(),
-      importante: true,
-    })
-    return note.save()
+
+ //Código para crear una nota nueva
+
+const note = new Note({
+  content: "Testing to grow better systems",
+  date: new Date(),
+  important: false,
+})
+
+note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close() //closes the collection
+}) 
+
+//Código para enlistar notas
+
+Note.find({}).then(result => {
+  result.forEach(note => {
+    console.log(note)
   })
-  .then(() => {
-    console.log('note saved!')
-    return mongoose.connection.close()
-  })
-  .catch((err) => console.log(err))
-  
+  mongoose.connection.close()
+})
+
+/* const allNotes = await Note.find();
+console.log(allNotes) */
+
+
+
+
